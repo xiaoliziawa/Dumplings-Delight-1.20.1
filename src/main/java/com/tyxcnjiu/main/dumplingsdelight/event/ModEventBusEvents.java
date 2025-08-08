1e7eec53b757df4907496ee8f3f01c996ffc310a
xiaoliziawa <3682822069@qq.com>
2025-08-08 23:31:32 +0800

    update

--- src/main/java/com/tyxcnjiu/main/dumplingsdelight/event/ModEventBusEvents.java
+++ src/main/java/com/tyxcnjiu/main/dumplingsdelight/event/ModEventBusEvents.java
@@ -2,9 +2,12 @@ package com.tyxcnjiu.main.dumplingsdelight.event;
 
 import com.tyxcnjiu.main.dumplingsdelight.DumplingsDelight;
 import com.tyxcnjiu.main.dumplingsdelight.event.loot.CalamariAdditionModifier;
+import com.tyxcnjiu.main.dumplingsdelight.registry.ItemRegistry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.ComposterBlock;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegisterEvent;
 
@@ -19,6 +22,26 @@ public class ModEventBusEvents {
                             CalamariAdditionModifier.CODEC);
                 }
         );
+    }
+
+    @SubscribeEvent
+    public static void commonSetup(FMLCommonSetupEvent event) {
+        event.enqueueWork(() -> {
+            // 种子(30%)
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.ChineseCabbageSeeds.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.EggplantSeeds.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.GarlicChiveSeeds.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.FennelSeeds.get(), 0.3f);
+            // 作物(65%)
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.ChineseCabbage.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.ChineseCabbageLeaf.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.Garlic.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.GarlicClove.get(), 0.3f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.GreenOnion.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.Eggplant.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.GarlicChive.get(), 0.65f);
+            ComposterBlock.COMPOSTABLES.put(ItemRegistry.Fennel.get(), 0.65f);
+        });
     }
 }
 
